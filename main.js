55ba26955c78e6ee9e0c336e7d7dd4da7693262d
Dosonov Aibol <you@aiboldosonob@gmail.com>
2024-05-10 01:52:26 +0600

    implement adding task functionality

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
 const submitBtn = document.querySelector('#submit__btn')
+const tasks = document.querySelector('.tasks')
+
 const task = document.querySelector('#task')
 // const submitBtn = document.querySelector('#submit__btn')
+
+
 submitBtn.addEventListener('click', (event)=>{
     event.preventDefault()
-    console.log(task.value)
+    if(task.value)
+    {
+        console.log(task.value)
+        const taskNew = document.createElement('div')
+        taskNew.className = 'task-container'
+        const input = document.createElement('input')
+        input.value = task.value
+        taskNew.appendChild(input)
+    
+        tasks.appendChild(taskNew)
+    }else
+    {
+        alert('write task')
+    }
+
 })
+
