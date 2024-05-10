9bc14e821e2c3741a641b51aa7736bfeac21f295
Dosonov Aibol <you@aiboldosonob@gmail.com>
2024-05-10 16:38:12 +0600

    implement removing task functionality

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const submitBtn = document.querySelector('#submit__btn')
+const editBtn = document.querySelector('.edit')
+const deleteBtn = document.querySelector('.delete')
+
 const tasks = document.querySelector('.tasks')
+const taskInputResult = document.querySelector('.task-result')
 
 const task = document.querySelector('#task')
 // const submitBtn = document.querySelector('#submit__btn')
@@ -9,12 +13,33 @@ submitBtn.addEventListener('click', (event)=>{
     event.preventDefault()
     if(task.value)
     {
-        console.log(task.value)
         const taskNew = document.createElement('div')
         taskNew.className = 'task-container'
+
         const input = document.createElement('input')
+        input.className = 'task-result'
         input.value = task.value
+        input.disabled = true
+
+        const taskEditBtn = editBtn.cloneNode('true')
+        const taskDeleteBtn = deleteBtn.cloneNode('true')
+
+
+        //Edit Button
+        taskEditBtn.addEventListener('click', (event)=>{
+            event.preventDefault()
+            input.disabled = false
+        })
+        
+        //Delete Button
+        taskDeleteBtn.addEventListener('click', (event)=>{
+            event.preventDefault()
+            taskNew.remove()
+        })
+
         taskNew.appendChild(input)
+        taskNew.appendChild(taskEditBtn)
+        taskNew.appendChild(taskDeleteBtn)
     
         tasks.appendChild(taskNew)
     }else
@@ -22,5 +47,10 @@ submitBtn.addEventListener('click', (event)=>{
         alert('write task')
     }
 
+})
+
+editBtn.addEventListener('click', (event)=>{
+    event.preventDefault()
+    taskInputResult.disabled = false
 })
 
